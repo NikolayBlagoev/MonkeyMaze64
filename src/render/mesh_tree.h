6017d1c5cac5554f71177f71505744803f5aacff
NikolayBlagoev <nickyblagoev@gmail.com>
2023-03-26 16:31:35 +0300

    And missing files :3

---
+++ src/render/mesh_tree.h
@@ -0,0 +1,38 @@
+#ifndef _MESH_TREE_H_
+#define _MESH_TREE_H_
+
+#include "mesh.h"
+#include <framework/disable_all_warnings.h>
+DISABLE_WARNINGS_PUSH()
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+DISABLE_WARNINGS_POP()
+#include <filesystem>
+#include <vector>
+
+
+
+class MeshTree {
+public:
+    MeshTree(GPUMesh* msh, glm::vec3 off, glm::vec3 rot, glm::vec3 scl);
+    int addChild(MeshTree* child);
+    MeshTree();
+    // void addMesh(std::filesystem::path filePath);
+    // void removeMesh(size_t idx);
+
+    // size_t numMeshes() { return meshes.size(); }
+    // const GPUMesh& meshAt(size_t idx) { return meshes[idx]; }
+
+    // glm::mat4 modelMatrix(size_t idx);
+
+    // std::vector<MeshTransform> transformParams;
+
+public:
+    GPUMesh* mesh;
+    glm::vec3 offset;
+    glm::vec3 rotate;
+    glm::vec3 scale;
+    std::vector<MeshTree*> children;
+};
+
+#endif
